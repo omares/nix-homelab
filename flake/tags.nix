11d86fb0ae0a4ae2d8aad25bf0f677620d4a45aa
Ota Mares <1375307+omares@users.noreply.github.com>
2026-01-31 22:22:38 +0100

    feat(bin/d): add zsh autocompletion, make .# prefix optional

--- flake/tags.nix
+++ flake/tags.nix
@@ -3,14 +3,18 @@
   config,
   ...
 }:
+let
+  nodes = config.mares.infrastructure.nodes;
+  managedNodes = lib.filterAttrs (_: cfg: cfg.managed or true) nodes;
+in
 {
-  flake.tags =
-    let
-      nodes = config.mares.infrastructure.nodes;
-      managedNodes = lib.filterAttrs (_: cfg: cfg.managed or true) nodes;
-      perNode = lib.mapAttrsToList (
-        name: cfg: lib.genAttrs (cfg.tags or [ ]) (_: [ name ])
-      ) managedNodes;
-    in
-    lib.zipAttrsWith (_: lib.flatten) perNode;
+  flake = {
+    nodes = lib.attrNames managedNodes;
+
+    tags =
+      let
+        perNode = lib.mapAttrsToList (name: cfg: lib.genAttrs (cfg.tags or [ ]) (_: [ name ])) managedNodes;
+      in
+      lib.zipAttrsWith (_: lib.flatten) perNode;
+  };
 }
